659c66e9b46f5c89c8b376304c90ea27b141fa6c
Diego Martinez <dmartinez-2026166@kinal.edu.gt>
2026-07-14 17:08:18 -0600

    Se modificaron los paquetes del proyecto

--- javafx-simulacion-login/src/com/diegomartinez/controller/ImageController.java
+++ javafx-simulacion-login/src/com/diegomartinez/controller/ImageController.java
@@ -1,5 +1,20 @@
 package com.diegomartinez.controller;
 
+import javafx.scene.image.Image;
 public class ImageController {
-    private final String RUTA_IMAGENES = "";
+
+    private final String RUTA_IMAGENES = "/com/diegomartinez/resources/";
+
+    public Image getImageLogin(String nombreImagen) {
+        Image crearImagen;
+        
+        switch (nombreImagen) {
+            case "logo":
+                crearImagen = new Image(RUTA_IMAGENES + "login-logo.png");
+                break;
+            default:
+                crearImagen = new Image(RUTA_IMAGENES + "login-logo.png");
+        }
+        return crearImagen;
+    }
 }
